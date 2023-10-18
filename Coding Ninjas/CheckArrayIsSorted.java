640c6ed05200300d64b5267d2408531387acb5cd
Ashwin B <121659162+GuruAshwin@users.noreply.github.com>
2023-10-18 14:26:36 +0530

    Create CheckArrayIsSorted.java

---
+++ Coding Ninjas/CheckArrayIsSorted.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static int isSorted(int n, int []a) {
+        if(n==1){
+            return 1;
+        }
+        for(int i=0;i<n-1;i++){
+            if(a[i]>a[i+1]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
