c9d91f270bb0946181cd7de73cc6a822ffa41d47
Ashwin B <121659162+GuruAshwin@users.noreply.github.com>
2023-10-18 14:08:56 +0530

    Update MissingNum286.java

--- Leetcode/MissingNum286.java
+++ Leetcode/MissingNum286.java
@@ -1,1 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int N=nums.length;
+        int sum=0;
+        for(int i=0;i<N;i++){
+            sum+=nums[i];
+        }
+        return (N*(N+1))/2-sum;
+    }
+}
+//Finding the total sum of array and sub with the fatorial of length of the array
 
